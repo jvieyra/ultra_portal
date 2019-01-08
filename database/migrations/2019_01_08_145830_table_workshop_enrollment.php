aafcb1d13ea4a1187bc6562c92202dd0bc70b77b
jvieyra <vieyrapez@gmail.com>
2019-01-08 09:01:31 -0600

    Creacion de clases e inscripciones workshops

---
+++ database/migrations/2019_01_08_145830_table_workshop_enrollment.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TableWorkshopEnrollment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshop_enrollment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('subject_id')->unsigned();
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->integer('year_id')->unsigned();
+            $table->foreign('year_id')->references('id')->on('years')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshop_enrollment');
+    }
+}
